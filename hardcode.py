c7f344e83d91768c8ec5ca3422eb3bfc6b930436
Nishant Rajpoot <134542404+nishantrajpoot719@users.noreply.github.com>
2025-06-04 11:14:09 +0530

    Update hardcode.py

--- hardcode.py
+++ hardcode.py
@@ -3,20 +3,20 @@ import random
 
 output1 = {
     "Emotion" : "Anxious",
-    "Top Products" : ["Taaza Jeera Chaach", "Dark Chocolate 55% - Nuts Infused", "Roasted Makhana - Cheese"],
-    "Top Combos" : ["Mango Yogurt & Muesli and Flakes - Chocolate Muesli", "Malabar Parata & Pickles", "Chips - Magic Masala - Potato Chips & No Maida Puffs - Disney Cream & Onion Puffs"]
+    "Products" : ["Taaza Jeera Chaach", "Dark Chocolate 55% - Nuts Infused", "Roasted Makhana - Cheese"],
+    "Combos" : ["Mango Yogurt & Muesli and Flakes - Chocolate Muesli", "Malabar Parata & Pickles", "Chips - Magic Masala - Potato Chips & No Maida Puffs - Disney Cream & Onion Puffs"]
 }
 
 output2 = {
     "Emotion": "Excited",
-    "Top Products": ["Roasted Makhana - Cheese","Taaza Jeera Chaach","Spicy Boondi"],
-    "Top Combos": ["Malabar Parata & Pickles","Taaza Jeera Chaach & Malabar Parata","Spicy Boondi & Malabar Parata"]
+    "Products": ["Roasted Makhana - Cheese","Taaza Jeera Chaach","Spicy Boondi"],
+    "Combos": ["Malabar Parata & Pickles","Taaza Jeera Chaach & Malabar Parata","Spicy Boondi & Malabar Parata"]
 }
 
 output3 = {
     "Emotion": "Content",
-    "Top Products": ["Taaza Pudina Masala Chaach","Mango Yogurt","Mango Lassi"],
-    "Top Combos": ["Taaza Pudina Masala Chaach & Muesli","Mango Yogurt & Muesli and Flakes - Muesli","Mango Lassi & Muesli"]
+    "Products": ["Taaza Pudina Masala Chaach","Mango Yogurt","Mango Lassi"],
+    "Combos": ["Taaza Pudina Masala Chaach & Muesli","Mango Yogurt & Muesli and Flakes - Muesli","Mango Lassi & Muesli"]
 }
 
 app = Flask(__name__)
